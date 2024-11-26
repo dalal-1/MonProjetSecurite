c483b3aca9d44793883d7cfeed92f2ad51bb3f22
dalal <dalaloumayma@gmail.com>
2024-11-26 21:06:53 +0000

    lkheeer

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template 
 from flask_bootstrap import Bootstrap
 print("Test de modification pour Jenkins")
-print("wa finallyyyyyyyy")
+print("wa finallyyyyyyyy aaayyyy")
 app=Flask(__name__)
 Bootstrap(app)
 @app.route('/')
