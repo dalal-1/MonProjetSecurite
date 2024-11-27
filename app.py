081adf7bd7c2985a75a51f5ec7db132fff5fe6c7
dalal <dalaloumayma@gmail.com>
2024-11-27 01:50:33 +0000

    pleaseee

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 print("Test de modification pour Jenkins")
 print("wa finallyyyyyyyy aaayyyy7")
 print("waa lakhdmat")
-print("lemail wsll")
+print("lemail wslll")
 app=Flask(__name__)
 Bootstrap(app)
 @app.route('/')
